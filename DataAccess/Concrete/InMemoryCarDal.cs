025ccb1bfceb60a88e6c3184825a4b61bad6c456
yesimdogan <yesimdevrimdogan@gmail.com>
2021-03-18 12:16:43 +0200

    Add project files.

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+
+            {
+                new Car{CarId = 1, BrandId = 1, ColorId=2, ModelYear=2010, DailyPrice=55000, Description=".."},
+                new Car{CarId = 2, BrandId = 2, ColorId=3, ModelYear=2012, DailyPrice=60000, Description="..."},
+                new Car{CarId = 3, BrandId = 4, ColorId=4, ModelYear=2016, DailyPrice=70000, Description="...."},
+                new Car{CarId = 4, BrandId = 3, ColorId=5, ModelYear=2019, DailyPrice=85000, Description="....."},
+                new Car{CarId = 5, BrandId = 5, ColorId=1, ModelYear=2020, DailyPrice=90000, Description="......"}
+            };
+
+
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.CarId == Id).ToList();
+        }
+
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Entities.Concrete
+{
+    public interface IEntity
+    {
+    }
+}
