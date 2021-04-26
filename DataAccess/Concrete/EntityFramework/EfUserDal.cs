a2ebce9aa2237ddde08f3fd0848b56bd231dd529
yesimdogan <yesimdevrimdogan@gmail.com>
2021-04-26 11:22:19 +0200

    jwt added.

--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using DataAccess.Concrete.EntityFramework;
 using Core.Entities.Concrete;
-using Entities.Concrete;
+
 
 namespace DataAccess.Concrete.EntityFramework
 {
